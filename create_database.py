8f5095a83e9b229d5ee5f2335df424a3f71310fc
V. Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2016-04-28 00:37:55 +0200

    python3-way sqlite

---
+++ create_database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('base.db')
+c = conn.cursor()
+
+def create_table():
+    c.execute('''CREATE TABLE IF NOT EXIST memory(
+              id integer primary key autoincrement,
+              user_id integer not null,
+              predicate text not null,
+              object text not null,
+              num real,
+              finished timestamp not null)''')
+conn.commit()
--- create_database.sql
+++
@@ -1,6 +0,0 @@
-create tabel memory(id integer primary key,
-                            user_id integer not null,
-                            predicate text not null,
-                            object text not null,
-                            num real,
-                            finished timestamp not null);
