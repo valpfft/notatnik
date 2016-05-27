5b119b18d6f0b7d3aa5a7068c91311f83ec2ac0a
V. Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2016-05-27 22:51:40 +0200

    add logging for debug. Some problems with connection. Temporary solution is set timeout for update in main loop ( bot. getUpdates(..., timeout = 10)

--- notatnik.py
+++ notatnik.py
@@ -1,35 +1,44 @@
 #! /usr/bin/python3
 
-
+import logging
 import datetime
 import re
-from create_database import conn
+from create_database import *
 from settings import bot
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s')
 
 
 def fun(database, bot, update):
     msg = update.message.text
     for_slice = [item for item in msg.split(' ')
-                 if item != u'Ja' and item != u'?']
+                 if item != u'ja' and item != u'?']
     cmd = for_slice.pop(0)
     predicate = ' '.join(for_slice)
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
     if cmd == u'co' and object == u'robiłem':
         bot.sendMessage(chat_id, prediacte_list(
-            database, user_id).encode('utf-8'))
+            database, user_id))
     if cmd in (u'co', u'kto', u'jak', u'gdzie',
                u'przypomni', u'działanie'):
         bot.sendMessage(chat_id,
-                        predicate_history(database, user_id, predicate))
+                        predicate_history(database,
+                                          user_id,
+                                          predicate))
     elif cmd in (u'ile', u'oblicz'):
         bot.sendMessage(chat_id,
-                        predicate_stats(database, user_id, predicate))
+                        predicate_stats(database,
+                                        user_id,
+                                        predicate))
     else:
         predicate, num = cmd, extract_number(object)
         bot.sendMessage(
             chat_id,
-            remember(database, user_id, predicate, object, num))
+            remember(database, user_id, predicate,
+                     object, num))
 
 
 def remember(database, user_id, predicate, object, num):
@@ -44,23 +53,25 @@ def remember(database, user_id, predicate, object, num):
 
 
 def prediacte_list(database, user_id, predicate):
-    database.execute('''SELECT predicate FROM memory''')
-    print(database.fetchall())
+    c = database.cursor()
+    c.execute('''SELECT predicate FROM memory''')
+    return c.fetchall()
 
 
 def predicate_history(database, user_id, predicate):
-    database.execute('''SELECT finished,
+    c = database.cursor()
+    c.execute('''SELECT finished,
                     predicate, object FROM memory  WHERE predicate = ? ''',
-                     (predicate,))
-    print(database.fetchall())
+              (predicate,))
+    return c.fetchall()
 
 
 def predicate_stats(database, user_id, predicate):
-    database.execute('''SELECT count(predicate), avg(predicate),
+    c = database.cursor()
+    c.execute('''SELECT count(predicate), avg(predicate),
                      object FROM memory WHERE predicate = ?
-                     GROUP BY object''', (predicate))
-    data = database.fetchall()
-    print(data)
+                     GROUP BY object''', (predicate,))
+    return c.fetchall()
 
 
 def extract_number(argument):
@@ -77,7 +88,7 @@ def extract_number(argument):
 with conn:
     last_update_id = bot.getUpdates()[-1].update_id
     while True:
-        for update in bot.getUpdates(offset=last_update_id):
+        for update in bot.getUpdates(offset=last_update_id, timeout=5):
             if last_update_id < update.update_id:
                 if update.message.text:
                     fun(conn, bot, update)
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import telegram
 # telegram token here
-TELEGRAM_TOKEN = ''
+TELEGRAM_TOKEN = '198842159:AAGe1r7PUlnKmhFb9CBPvH5GyGXIQhzeDZs'
 bot = telegram.Bot(TELEGRAM_TOKEN)
