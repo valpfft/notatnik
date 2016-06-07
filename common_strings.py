4d0a24b6890a2bf4472a6a8b9ea4c3223249ede1
V. Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2016-06-08 00:16:54 +0200

    added help message

---
+++ common_strings.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+HELP = str(u''' "/help message :"
+        Ten bot zapamięta, co robicie,
+        a następnie może powiedzieć o tym.
+        Aby dodać nowy rekord, wystarczy napisać co zrobiłeś.
+        Naprzyklad tak:
+
+        przeczytałem Dive into python
+
+        lub
+
+        Przebiegłem 3 km
+
+        lub
+
+        Ja ważę 55 kilo
+
+        Teraz, jeśli go zapytać: "co przeczytałem", Bot wyświetli listę
+        przeczytanych książek. Na pytanie "ile przebiegłem" policzy
+        średnią odległość i narysuje wykres.
+        Ponadto, można zapytać "Pomóc" lub "co ja robiłem"
+        Zamiast słowa 'co', można użyć 'kto', 'jak', 'gdzie'.
+        A 'ile' można zamienić na 'oblicz'
+        ''')
