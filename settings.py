456f4aee6ac4e44f81c8c992af7b7f6ddc31607f
V. Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2016-06-08 00:29:16 +0200

    added settings for Updater method

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python3
-import telegram.ext
+from telegram.ext import Updater
+import telegram
 # telegram token here
 TELEGRAM_TOKEN = '198842159:AAGe1r7PUlnKmhFb9CBPvH5GyGXIQhzeDZs'
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
+updater = Updater(token=TELEGRAM_TOKEN)
+dispatcher = updater.dispatcher
+face_api_key = 'a9c890353dbb41a4a34f8315ab593eee'
