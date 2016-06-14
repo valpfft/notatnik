f3806297a63923743767b413d59f2753cf3b7113
V. Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2016-06-14 02:08:05 +0200

    added loop for photo type messages

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ from flask import Flask, g, request, url_for
 import notatnik
 from settings import bot, updater, dispatcher
 from database import create_table, conn, cursor
+from face_detection import face_detection
 
 app = Flask(__name__)
 
 with conn:
     try:
-        last_update_id = bot.getUpdates()[-1].update_id
+        last_update_id = bot.getUpdates()[0].update_id
     except IndexError:
         last_update_id = 0
     while True:
@@ -17,6 +18,13 @@ with conn:
             if last_update_id < update.update_id:
                 if update.message.text:
                     notatnik.fun(cursor, bot, update)
+                    last_update_id = update.update_id
+                elif update.message.photo:
+                    chat_id = update.message.chat_id
+                    user_id = update.message.from_user.id
+                    photo_url = update.message.photo[-1].file_id
+                    file = bot.get_file(photo_url)
+                    bot.sendMessage(chat_id, face_detection(file.file_path))
                     last_update_id = update.update_id
 
 
@@ -43,4 +51,5 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0',
             port=environ.get('PORT', 5000),
             debug=False,
-            process=2)
+            process=2,
+            server='gunicorn')
